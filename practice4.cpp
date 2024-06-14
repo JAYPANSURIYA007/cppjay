4ad1069aa722da040f372f2b7d5a1d3d5b19ecff
Yash <ustavpatel2004@gmail.com>
2024-06-14 11:36:41 +0530

    tgh

---
+++ practice4.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=1;i<=10;i++){
+        cout<<int(i)<<"\t";
+    }
+    cout<<endl;
+    for(int i=10;i>0;i--){
+        cout<<int(i)<<"\t";
+    }
+}
