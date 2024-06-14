40d1bdfa2cbabfba6f4781db67701770fccf9f37
Yash <ustavpatel2004@gmail.com>
2024-06-14 11:04:43 +0530

    ghfg

--- practice2.cpp
+++ practice2.cpp
@@ -5,6 +5,16 @@ int main(){
     t1=0;
     t2=1;
     nextterm=t1+t2;
+    cout<<"enter the number:";
+    cin>>n;
+    cout<<int(t1)<<endl<<int(t2)<<endl;
+
+    for(int i=3;i<=n;i++){
+        cout<<int(nextterm)<<endl;  
+        t1=t2;
+        t2=nextterm;
+        nextterm =t1+t2;
+         }
 
 
 }
