b851da406d9a287b671b03765e440a76a7512db9
Yash <ustavpatel2004@gmail.com>
2024-06-15 11:52:38 +0530

    fvgjylh

---
+++ practiuce11.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+
+    void sum();
+    int sum1(){
+        float l,a;
+        cout<<"enter the l:";
+        cin>>l;
+        a=l*l;
+        return a;
+    }
+    int main(){
+        int total;
+        total=sum1();
+        cout<<"the total of  a is:"<< total ;
+
+    }
+
+
+    
