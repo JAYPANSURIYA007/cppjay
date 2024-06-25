ca4c77eee6fffd2a7f63cf036465b3a651cd0613
Yash <ustavpatel2004@gmail.com>
2024-06-25 11:37:58 +0530

    erh

---
+++ operateroverlloading2.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+class complex{
+      
+      int x,y;
+      public:
+      complex(){
+        x=0;
+        y=0;
+
+      }
+       complex(int a , int b){
+        x=a;
+        y=b;
+       }
+      void desplay(){
+
+     
+        cout<<"value of x:"<<x<<endl;
+        cout<<"value of y:"<<y<<endl;
+      }
+      
+      complex operator-(complex e){
+        complex t;
+        t.x=x-e.x;
+        t.y=y-e.y;
+        return t;
+      }
+};
+int main(){
+    complex s1,s2,s3;
+    s1=complex(10,20);
+    s2=complex(5,10);
+    cout<<"first object"<<endl;
+    s1.desplay();
+    cout<<"secound object"<<endl;
+    s2.desplay();
+    cout<<"final output"<<endl;
+    s3=s1-s2;
+    s3.desplay();
+}
